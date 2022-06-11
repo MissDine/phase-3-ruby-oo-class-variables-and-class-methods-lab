15c9ad8591689170bb438b6b87bc37383e01c7a5
MissDine <enidnyatichi1212@gmail.com>
2022-06-11 15:33:50 +0300

    Completed assignment

--- lib/song.rb
+++ lib/song.rb
@@ -1,32 +1,55 @@
 class Song
-    @@genre_count = 0
+    # Macro methods
+    attr_accessor :name, :artist, :genre
+  
+    #class methods
     @@artists = []
     @@genres = []
-    attr_accessor :name, :artist, :genre
-
-    def initialize(name, artist, genre)
-        @name = name,
-        @artist = artist,
-        @genre = genre
-
-        @@genre_count += 1
-        @@genres << genre
-        @@artists << artist
-    end 
-    
-    def hit_me
-        puts "hit me baby one more time"
-    end    
-    
-    def self.genre_count
-        "#{@@genre_count}"
-    end 
-    
-    def self.artists
-        @@artists.uniq
-    end 
-    
-    def self.genres
-        @@genres.uniq
+    @@count = 0
+    @@artist_count = {}
+    @@genre_count = {}
+  
+  
+  
+    def initialize(name, singer, genre)
+      @name = name
+      @artist = singer
+      @genre = genre
+      @@count += 1
+      @@artists << @artist
+      @@genres << @genre
     end
-end    
+  
+    def self.artists
+      @@artists.uniq!
+    end
+  
+    def self.genres
+      @@genres.uniq!
+    end
+    
+    def self.count
+      @@count
+    end
+  
+    def self.artist_count
+      @@artist_count.default = 0
+  
+      @@artists.each { |artist| @@artist_count[artist] += 1}
+  
+      @@artists.each {|a| "#{a} : #{@@artist_count[a]}" }
+      @@artist_count
+    end
+  
+    def self.genre_count
+      @@genre_count.default = 0
+      @@genres.each{|genre| @@genre_count[genre] +=1}
+      # @@genres.each {}
+      @@genre_count
+    end
+  
+  end
+  
+  Song.new("Lucifer", "Jay-Z", "rap")
+  Song.new("99 Problems", "Jay-Z", "rap")
+  Song.new("hit me baby one more time", "Brittany Spears", "pop")
